365462f8c9220d038d7ad48b6c8e50425530d7cb
Marcosdsj <mfsilva5585@gmail.com>
2024-08-17 01:36:38 -0300

    add metodo de selecaoCandidatos

--- controle-candidatos/controleCandidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/controleCandidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,8 +1,34 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        System.out.println("processo seletivo");
+       // System.out.println("processo seletivo");
+       selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos(){
+
+            String [] candidatos = {"Felipe", "Marcia", "Julia","Paulo","Augusto","Mônica","Fabricio","Dany"};
+
+            int candidatosSelecionados = 0;
+            int candidatosAtual = 0;
+            double salarioBase = 2000.0;
+            while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+                String candidato = candidatos[candidatosAtual];
+                double salarioPretendido = valorPretendido();
+
+                System.out.println("O candidato "+ candidato +" solicitou este valor de salário "+ salarioPretendido);
+                if (salarioBase >= salarioPretendido) {
+                    System.out.println("O candidato "+ candidato +" foi selecionado para a vaga");
+                    candidatosSelecionados++;
+                }
+                candidatosAtual++;
+            }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido){
