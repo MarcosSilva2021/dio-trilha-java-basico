d9b64ccd226b895a05b621401bd8a6aa4b746ac2
Marcosdsj <mfsilva5585@gmail.com>
2024-08-30 16:18:45 -0300

    update classe Iphone

--- DesafioIphone/desafioIphone/src/Iphone.java
+++ DesafioIphone/desafioIphone/src/Iphone.java
@@ -1,5 +1,50 @@
-public class Iphone {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Iphone implements Telefone, NavegadorInternet, ReprodutorMusica {
+    public static void main(String[] args){
+        System.out.println("Classe Iphone");
+    }
+
+    @Override
+    public void tocar() {
+         //Implementação aqui
+    }
+
+    @Override
+    public void pausar() {
+         //Implementação aqui
+    }
+
+    @Override
+    public void selecionarMusica() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void exibirPagina() {
+         //Implementação aqui
+    }
+
+    @Override
+    public void adicionarPagina() {
+        //Implementação aqui
+    }
+
+    @Override
+    public void atualizarPagina() {
+         //Implementação aqui
+    }
+
+    @Override
+    public void ligar(String numero) {
+        //Implementação aqui
+    }
+
+    @Override
+    public void atender(String numero) {
+         //Implementação aqui
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String mensagem) {
+        //Implementação aqui
     }
 }
