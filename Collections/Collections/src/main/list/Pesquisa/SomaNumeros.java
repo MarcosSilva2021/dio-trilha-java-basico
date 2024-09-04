459555c9ed713e929b08816a1089e1f2992d5a2d
Marcosdsj <mfsilva5585@gmail.com>
2024-09-03 22:04:22 -0300

    add class somaNumeros

---
+++ Collections/Collections/src/main/list/Pesquisa/SomaNumeros.java
@@ -0,0 +1,42 @@
+package main.list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaNumeros {
+    // atributo lista de numeros
+    private List<Integer> numeros;
+
+    public SomaNumeros() {
+        this.numeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero){
+        this.numeros.add(numero);
+    }
+
+    public int calcularSoma(){
+        int soma = 0;
+        for (Integer numero : numeros){
+            soma += numero;
+        }
+        return soma;
+    }
+
+    public int encontrarMaiorNumero(){
+        int maiorNumero = Integer.MIN_VALUE;
+        if (!numeros.isEmpty()) {
+            for (Integer numero : numeros){
+                if (numero >= maiorNumero){
+                    maiorNumero = numero;
+                }
+            }
+            return maiorNumero;
+        }else {
+            throw new RuntimeException("A lista está vazia");
+        }
+    }
+
+    
+
+}
