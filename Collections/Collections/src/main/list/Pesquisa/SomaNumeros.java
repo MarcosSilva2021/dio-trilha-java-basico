75c68fe3bfca1dbe79825affb4ea29125ddf4675
Marcosdsj <mfsilva5585@gmail.com>
2024-09-04 15:59:55 -0300

    add metodo teste

--- Collections/Collections/src/main/list/Pesquisa/SomaNumeros.java
+++ Collections/Collections/src/main/list/Pesquisa/SomaNumeros.java
@@ -37,6 +37,51 @@ public class SomaNumeros {
         }
     }
 
-    
+    public int encontrarMenorNumero(){
+        int menorNumero = Integer.MAX_VALUE;
+        if (!numeros.isEmpty()) {
+            for (Integer numero : numeros){
+                if (numero <= menorNumero){
+                    menorNumero = numero;
+                }
+            }
+            return menorNumero;
+        }else{
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
 
+    public void exibirNumeros(){
+        if (!numeros.isEmpty()) {
+            System.out.println(this.numeros);            
+        }else{
+            System.out.println("A lista está vazia !!!");
+        }
+    }
+
+    public static void main(String[] args) {
+        // criou instancia da classe SomaNumeros
+        SomaNumeros somaNumeros = new SomaNumeros();
+
+        // add numeros a lista
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(-3);
+        somaNumeros.adicionarNumero(3);
+        somaNumeros.adicionarNumero(12);
+
+        // exibir a lista de numeros
+        System.out.println("Numeros adicionados: ");
+       somaNumeros.exibirNumeros();
+
+       // calcular e exibir a soma dos numeros na lista
+       System.out.println("Soma dos numeros: " + somaNumeros.calcularSoma());
+
+       // calcular e exibir a maior numeros na lista
+       System.out.println("Maior numero: " + somaNumeros.encontrarMaiorNumero());
+
+       // calcular e exibir a menor numeros na lista
+       System.out.println("Menor numero: " + somaNumeros.encontrarMenorNumero());
+    }         
 }
