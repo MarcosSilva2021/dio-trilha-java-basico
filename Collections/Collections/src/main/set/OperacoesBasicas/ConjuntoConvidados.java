ef87108b8a94fed0b2d14386f70f19ef1593e1af
Marcosdsj <mfsilva5585@gmail.com>
2024-09-05 16:23:30 -0300

    create class ConjConvidado

---
+++ Collections/Collections/src/main/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,42 @@
+package main.set.OperacoesBasicas;
+
+import java.util.Set;
+import java.util.HashSet;
+
+
+public class ConjuntoConvidados {
+    // atributo
+    private Set<Convidado> convidadosSet;
+
+    public ConjuntoConvidados() {
+        this.convidadosSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadosSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        if (! convidadosSet.isEmpty()) {
+            for (Convidado c : convidadosSet){
+                if (c.getCodigoConvite() == codigoConvite) {
+                    convidadoParaRemover = c;
+                    break;
+                }
+            }
+            convidadosSet.remove(convidadoParaRemover);
+        } else {
+            throw new RuntimeException("O conjunto está vazio !!");
+        }
+    }
+
+    public int contaConvidado(){
+        return convidadosSet.size();
+    }
+
+
+
+    
+
+}
