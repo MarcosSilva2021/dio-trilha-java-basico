ead975efc42f318df1907685d59f0239efdc13fd
Marcosdsj <mfsilva5585@gmail.com>
2024-09-07 20:52:03 -0300

    add class Tarefa

---
+++ Collections/Collections/src/main/set/Pesquisa/Tarefa.java
@@ -0,0 +1,39 @@
+package main.set.Pesquisa;
+
+public class Tarefa {
+    // atributos
+    private String descricao;
+    private boolean concluida;
+    
+    public Tarefa(String descricao, boolean concluida) {
+        this.descricao = descricao;
+        this.concluida = concluida;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    @Override
+    public String toString() {
+        return "[descricao=" + descricao + ", concluida=" + concluida + "]";
+    }
+
+    
+
+    
+    
+
+}
