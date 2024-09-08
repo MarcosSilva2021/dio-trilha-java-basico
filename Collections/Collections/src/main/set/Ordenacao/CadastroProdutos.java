2ab63389fdaf92b04d6108766d77ede0040dd1ff
Marcosdsj <mfsilva5585@gmail.com>
2024-09-08 20:03:13 -0300

    add metodo teste

--- Collections/Collections/src/main/set/Ordenacao/CadastroProdutos.java
+++ Collections/Collections/src/main/set/Ordenacao/CadastroProdutos.java
@@ -1,7 +1,8 @@
 package main.set.Ordenacao;
 
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class CadastroProdutos {
     // atributo
@@ -15,6 +16,40 @@ public class CadastroProdutos {
         produtoSet.add(new Produto(codigo, nome, preco, quantidade));
     }
 
-    
-    
+    public Set<Produto> exibirProdutosPorNome(){
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        if (!produtoSet.isEmpty()) {
+            return produtosPorNome;
+        } else {
+            throw new RuntimeException("O conjunto está vazio !");
+        }
+    }
+
+    public Set<Produto> exibiProdutosPorPreco(){
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparotorPorPreco());
+        if (!produtoSet.isEmpty()) {
+            produtosPorPreco.addAll(produtoSet);
+            return produtosPorPreco;
+        } else {
+            throw new RuntimeException("O conjunto está vazio");
+        }
+    }
+
+    public static void main(String[] args) {
+        // criando uma instancia da classe cadastroProduto
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        // add produtos ao cadastro
+        cadastroProdutos.adicionarProduto(1L, "Smartphone", 1000d, 12);
+        cadastroProdutos.adicionarProduto(2l, "Utrabook", 150d, 9);
+        cadastroProdutos.adicionarProduto(1L, "Tablet", 15d, 43);
+        cadastroProdutos.adicionarProduto(4l, "Tela 4k", 45d, 15);
+
+        //exibir todos os produtos cadastrados
+        System.out.println("\nExibir produtos cad: "+cadastroProdutos.produtoSet);
+
+        System.out.println("\n ordenar por nome: "+ cadastroProdutos.exibirProdutosPorNome());
+
+        System.out.println("\n Ordem por Preço: "+ cadastroProdutos.exibiProdutosPorPreco());
+    }    
 }
