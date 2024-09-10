887a3c5600d7ce000a499fa68c51153c167f1b5a
Marcosdsj <mfsilva5585@gmail.com>
2024-09-10 11:41:01 -0300

    add metodos addPalavras, removerPalavras

--- Collections/Collections/src/main/map/OperacoesBasicas/Dicionario.java
+++ Collections/Collections/src/main/map/OperacoesBasicas/Dicionario.java
@@ -1,5 +1,29 @@
 package main.map.OperacoesBasicas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Dicionario {
+    private Map<String, String> dicionario;
+
+    public Dicionario() {
+        this.dicionario = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, String definicao){
+        dicionario.put(palavra, definicao);
+    }
+
+    public void removerPalavra(String palavra){
+        if (! dicionario.isEmpty()) {
+            dicionario.remove(palavra);
+        } else {
+            System.out.println("O dicionario está vazio. ");
+        }
+    }
+
+    
+
+    
 
 }
