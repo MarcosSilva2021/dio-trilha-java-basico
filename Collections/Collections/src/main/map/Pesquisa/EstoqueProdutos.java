85846f789ef5a1a27ba1744ce538cc978f4f297f
Marcosdsj <mfsilva5585@gmail.com>
2024-09-10 15:52:05 -0300

    add class EstoqueProdutos

---
+++ Collections/Collections/src/main/map/Pesquisa/EstoqueProdutos.java
@@ -0,0 +1,15 @@
+package main.map.Pesquisa;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class EstoqueProdutos {
+    private Map<Long, Produto> EstoqueProdutosMap;
+
+    public EstoqueProdutos(){
+        this.EstoqueProdutosMap = new HashMap<>();
+    }
+
+
+
+}
