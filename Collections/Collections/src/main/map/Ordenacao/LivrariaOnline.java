d85f0330b4b8d833064a6c798eed75a6196df289
Marcosdsj <mfsilva5585@gmail.com>
2024-09-11 16:14:42 -0300

    add metodo comparator LivrosPorpreco

--- Collections/Collections/src/main/map/Ordenacao/LivrariaOnline.java
+++ Collections/Collections/src/main/map/Ordenacao/LivrariaOnline.java
@@ -1,7 +1,9 @@
 package main.map.Ordenacao;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -23,7 +25,22 @@ public class LivrariaOnline {
                 chavesRemover.add(entry.getKey());
             }
         }
+        for (String chave : chavesRemover){
+            livros.remove(chave);
+        }
     }
+
+    public Map<String, Livro> exibirLivrosOrdenadosPorPreco(){
+        List<Map.Entry<String, Livro>> livrosParaOrdenarPorPreco = new ArrayList<>(livros.entrySet());
+        Collections.sort(livrosParaOrdenarPorPreco, new ComparatorPorPreco());
+        Map<String, Livro> livrosOrdenadosPorPreco = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Livro> entry : livrosParaOrdenarPorPreco){
+            livrosOrdenadosPorPreco.put(entry.getKey(), entry.getValue());
+        }
+        return livrosOrdenadosPorPreco;
+    }
+    
 
     
 
