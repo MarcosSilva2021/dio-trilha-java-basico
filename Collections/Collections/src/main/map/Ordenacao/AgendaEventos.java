9f1238e65dfaf2a97725fcf2508556a163d428dd
Marcosdsj <mfsilva5585@gmail.com>
2024-09-11 14:31:34 -0300

    add metodo de obterProximoEventos

--- Collections/Collections/src/main/map/Ordenacao/AgendaEventos.java
+++ Collections/Collections/src/main/map/Ordenacao/AgendaEventos.java
@@ -3,6 +3,7 @@ package main.map.Ordenacao;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class AgendaEventos {
     private Map<LocalDate, Evento> eventosMap;
@@ -15,6 +16,33 @@ public class AgendaEventos {
         eventosMap.put(data, new Evento(nomeEvento, descricaoAtracao));
     }
 
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()){
+            LocalDate dataEvento = entry.getKey();
+            Evento evento = entry.getValue();
+            System.out.println("Data: " + dataEvento + ", Evento: "+ evento.getNome() + ", Atração: "+ evento.getAtracao());
+        }
+    }
+
+    public void obterProximoEvento(){
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximoData = null;
+        Evento proximoEvento = null;
+        for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()){
+            LocalDate dataEvento = entry.getKey();
+            if (dataEvento.isEqual(dataAtual) || dataEvento.isAfter(dataAtual)) {
+                proximoData = dataEvento;
+                proximoEvento = entry.getValue();
+                break;
+            }
+        }
+        if (proximoEvento != null) {
+            System.out.println("O proximo evento: "+ proximoEvento.getNome() + " acontecerá na data " + proximoData);
+        } else {
+            System.out.println("Não foram encontradas eventos futuros na agenda");
+        }
+    }
     
 
 }
