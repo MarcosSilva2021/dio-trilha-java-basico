906013c36e819711c0ebbcca2e0fc65b1e0401c4
Marcosdsj <mfsilva5585@gmail.com>
2024-09-11 11:16:55 -0300

    add metodo add e remove Palavras

--- Collections/Collections/src/main/map/Pesquisa/ContagemPalavras.java
+++ Collections/Collections/src/main/map/Pesquisa/ContagemPalavras.java
@@ -1,9 +1,28 @@
 package main.map.Pesquisa;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ContagemPalavras {
     // atributos
     private Map<String, Integer> palavras;
+
+    public ContagemPalavras() {
+        this.palavras = new HashMap<>();
+    }
+
+    public void adicionarPalavras(String linguagem, Integer contagem ){
+        palavras.put(linguagem, contagem);
+    }
+
+    public void removerPalavras(String palavra){
+        if (!palavras.isEmpty()) {
+            palavras.remove(palavra);
+        } else {
+            System.out.println("O conjunto Map está vazio !!");
+        }
+    }
+
+    
 
 }
