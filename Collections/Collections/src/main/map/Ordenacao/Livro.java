4902def149270f22cf28775d264c5721b60b6f52
Marcosdsj <mfsilva5585@gmail.com>
2024-09-11 14:47:28 -0300

    add class Livro

---
+++ Collections/Collections/src/main/map/Ordenacao/Livro.java
@@ -0,0 +1,19 @@
+package main.map.Ordenacao;
+
+public class Livro {
+    // atributos
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    
+
+    
+
+}
