b7e8b391ed25680c00ac6960287575a140473985
Marcosdsj <mfsilva5585@gmail.com>
2024-09-14 22:49:27 -0300

    add class cliente

--- DesafioBancoDigitalComJavaOO/src/main/java/dio/desafioCriandoUmBancoDigital/Cliente.java
+++ DesafioBancoDigitalComJavaOO/src/main/java/dio/desafioCriandoUmBancoDigital/Cliente.java
@@ -2,4 +2,21 @@ package main.java.dio.desafioCriandoUmBancoDigital;
 
 public class Cliente {
 
+    private String nome;
+    private String cpf;
+    private String profissao;
+    private String email;
+    private String telefone;
+    
+    public Cliente(String nome, String cpf, String profissao, String email, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.profissao = profissao;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    
+
+
 }
