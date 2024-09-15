55bdd13d294629660031c0688999de80efb4acdc
Marcosdsj <mfsilva5585@gmail.com>
2024-09-14 22:15:38 -0300

    add class Banco

--- DesafioBancoDigitalComJavaOO/src/main/java/dio/desafioCriandoUmBancoDigital/Banco.java
+++ DesafioBancoDigitalComJavaOO/src/main/java/dio/desafioCriandoUmBancoDigital/Banco.java
@@ -1,5 +1,43 @@
 package main.java.dio.desafioCriandoUmBancoDigital;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Banco {
+    // atributos
+    private String nome;
+    private List<Conta> contas;
+    
+    List<Cliente> clientes = new ArrayList<Cliente>();
+
+    // construtor
+    public Banco() {
+    }
+
+    // getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
+
+    public void mostrarClientes(){
+        System.out.println(clientes);
+    }
+
+    
 
 }
